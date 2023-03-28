d5091fabb0f982293a00a6984118263a7690e72f
Waqas <125983247+MuhammadWaqas3@users.noreply.github.com>
2023-03-28 21:47:49 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Showprofile from './Components/ShowProfie';
+
+function App() {
+    return (
+        <div>
+            <Showprofile/>
+        </div>
+    )
+}
+
+export default App;
